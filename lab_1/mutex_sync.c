98c8e31b9444d5c937e4cf0b2ddd35927e09773e
maresyp <mattq.2000@icloud.com>
2021-11-29 18:09:18 +0100

    add mutex_sync.c

---
+++ lab_1/mutex_sync.c
@@ -0,0 +1,41 @@
+//
+// Created by Mateusz Pysera on 29/11/2021.
+//
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+
+    // TODO: handle envp
+
+    if (argc == 1) {
+        printf("No arguments specified");
+    } else {
+
+        pthread_mutex_t mutex;
+        pthread_mutexattr_t attr_mutex;
+
+        pthread_mutexattr_init(&attr_mutex);
+        pthread_mutexattr_setpshared(&attr_mutex, PTHREAD_PROCESS_SHARED);
+
+        pthread_mutex_init(&mutex, &attr_mutex);
+
+        for (int i = 0; i < argc; ++i) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                // do child stuff
+                pthread_mutex_lock(&mutex);
+                printf("%d %s\n", i, argv[i]);
+                pthread_mutex_unlock(&mutex);
+                return 0;
+            }
+        }
+
+        pthread_mutex_destroy(&mutex);
+        pthread_mutexattr_destroy(&attr_mutex);
+    }
+    getchar(); // stop here
+    return 0;
+}
