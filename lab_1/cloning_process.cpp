c8ebebee61df4371b3b40cb97b5f0695a646aeff
maresyp <mattq.2000@icloud.com>
2021-12-09 18:01:07 +0100

    init

--- lab_1/cloning_process.cpp
+++ lab_1/cloning_process.cpp
@@ -1,4 +1,15 @@
 //
 // Created by Mateusz Pysera on 28/11/2021.
 //
-
+#include <iostream>
+#include <unistd.h>
+void prt() {
+    fork();
+    std::cout << "fork" << std::endl;
+}
+int main() {
+    std::cout << "Hello before fork" << std::endl;
+    prt();
+    std::cout << "Hello after fork" << std::endl;
+    return 0;
+}
