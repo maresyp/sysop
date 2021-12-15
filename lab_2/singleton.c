4ae83d12c80712c8ac335550130a1e9a91badc81
maresyp <mattq.2000@icloud.com>
2021-12-15 20:10:41 +0100

    clone change

--- lab_2/singleton.c
+++ lab_2/singleton.c
@@ -1,15 +1,18 @@
 //
 // Created by Mateusz Pysera on 12/12/2021.
 //
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
+#include <semaphore.h>
 
-int main(int argc, char *argv[], char **envp) {
+int main(int argc, char *argv[], char *envp[]) {
 
+    // create named semaphore
+    sem_open("/my_semaphore", 1);
 
     int flag = 0;
     for (char **env = envp; *env != 0; env++) {
-        char *thisEnv = *env;
+        const char *thisEnv = *env;
         if (strcmp(thisEnv, "SO2=NEW") == 0) {
             flag = 1;
         }
