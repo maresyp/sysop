a7298b09cba423f1a0bed45a4b793f8324f411a5
maresyp <mattq.2000@icloud.com>
2021-12-15 13:40:47 +0100

    fix

--- lab_2/fork_clone.c
+++ lab_2/fork_clone.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <unistd.h> // for fork
 #include <stdio.h>
 #include <string.h>
@@ -5,8 +7,10 @@
 #include <sched.h>
 #include <sys/wait.h>
 
-void duplicate_me(void *arg) {
+int duplicate_me(void *arg) {
+    printf("Duplicate me is running");
     printf("clone arg -> %s", (char *) arg);
+    return 0;
 }
 
 int main(int argc, char *argv[], char *envp[]) {
@@ -16,17 +20,23 @@ int main(int argc, char *argv[], char *envp[]) {
         if (strcmp(argv[1], "c") == 0) {
             // use clone
             const int STACK_SIZE = 65536;
-            char *stack = malloc(STACK_SIZE);
+            void *stack = malloc(STACK_SIZE);
             if (stack == NULL) {
                 printf("bad alloc");
                 exit(-1);
             }
             for (int i = 2; i < argc; ++i) {
-                pid_t pid = clone(duplicate_me, stack, SIGCHLD, (void *) argv[i]);
+                pid_t pid = clone(duplicate_me, stack + STACK_SIZE, SIGCHLD, argv[i]);
+                if (pid == -1) {
+                    printf("Failed to clone");
+                    exit(EXIT_FAILURE);
+                }
+                printf("New process using clone created with pid = %d\n", pid);
             }
+            free(stack);
         } else {
             // use fork
-            for (int i = 2; i < argc; ++i) {
+            for (int i = 1; i < argc; ++i) {
                 pid_t pid = fork();
                 if (pid == -1) { printf("Forking failed"); }
                 if (pid == 0) {
