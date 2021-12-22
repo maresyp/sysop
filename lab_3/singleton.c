51f2ebe22d23801d024ce4b9267706206e6a70a8
maresyp <mattq.2000@icloud.com>
2021-12-22 18:12:45 +0100

    update

--- lab_3/singleton.c
+++ lab_3/singleton.c
@@ -3,13 +3,31 @@
 
 
 int main(int argc, char * argv[], char * envp[]) {
+
+    // try to open file in write mode
+    // this should fail if other process is running
+    FILE * fptr = fopen("file_lock.txt", "w");
+    if (fptr == NULL) {
+        printf("Failed to open file_lock");
+        exit(EXIT_FAILURE);
+    }
+    fclose(fptr);
+
     HANDLE file_handle = CreateFile(
-        TEXT("datafile.txt"),
+        TEXT("file_lock.txt"),
         GENERIC_READ | GENERIC_WRITE,
-        0,          // open with exclusive access
+        FILE_SHARE_READ, // other process can read file     
         NULL,       // no security attributes
         CREATE_NEW, // creating a new temp file
         0,          // not overlapped index/O
         NULL);
+    
+    printf("%d", GetLastError());
+    getchar();
+
+    if (!CloseHandle(file_handle)) {
+        printf("Something went wrong while closing file handle");
+        exit(EXIT_FAILURE);
+    }
     return 0;
 }
