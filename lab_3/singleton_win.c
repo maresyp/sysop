ab1ecca476921c8d218ffba15be6182cf2a3ec4a
maresyp <mattq.2000@icloud.com>
2021-12-25 15:59:56 +0100

    update

--- lab_3/singleton_win.c
+++ lab_3/singleton_win.c
@@ -16,22 +16,29 @@ int main(int argc, char * argv[], char * envp[]) {
     if (GetLastError() == ERROR_ALREADY_EXISTS) {
         printf("Another process is running\n");
 
-        // TODO : check for env variable 
+        // check for env variable 
         char * env = getenv("SO2");
-        if (env != NULL && strcmp(env, "NEW") == 0) {
-            printf("Env -> %s", env);
+        if (env != NULL) {
+            printf("SO2=%s\n", env);
+            if (strcmp(env, "NEW") == 0) {
+                printf("Terminating previous process ...\n");
 
-            // wait for previous process to terminate and grab mutex
-            WaitForSingleObject(mutex, INFINITE);    
+                // TODO : Read pid from fille and kill process
+
+                // wait for previous process to terminate and grab mutex
+                WaitForSingleObject(mutex, INFINITE);   
+            }
         } else { 
             exit(EXIT_FAILURE);
         }
     }
 
+    printf("Created new process with pid=%d", GetCurrentProcessId());
+    
     // TODO: Write current pid to file
 
     getchar();
     CloseHandle(mutex);
     
-    return 0;
+    exit(EXIT_SUCCESS);
 }
